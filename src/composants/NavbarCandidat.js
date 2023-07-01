1d47f85a2082d456b04ce7eb78c67521dac8742b
unknown <fycityplay@gmail.com>
2023-07-01 12:17:29 +0300

    ito

--- src/composants/NavbarCandidat.js
+++ src/composants/NavbarCandidat.js
@@ -2,64 +2,32 @@ import React from 'react'
 import { NavLink, NavLinkProps } from 'react-router-dom';
 const NavbarCandidat = () => {
   return (
-    <main id="main-container">
-        
-    <div className="bg-success">
-      <div className="bg-black-10">
-        <div className="content py-3">
-      
-          <div className="d-lg-none">
-            <button type="button" className="btn w-100 btn-alt-secondary d-flex justify-content-between align-items-center"
-              data-toggle="className-toggle" data-target="#main-navigation" data-className="d-none">
-              Menu
-              <i className="fa fa-bars"></i>
-            </button>
-          </div>
-
-          <div id="main-navigation" className="d-none d-lg-block mt-2 mt-lg-0">
-            <ul className="nav-main nav-main-dark nav-main-horizontal nav-main-hover">
-              <li className="nav-main-item">
-                <a className="nav-main-link" href="#">
-                  <span className="nav-main-link-name active">Menu</span>
-                </a>
-              </li>
-              <li className="nav-main-item">
-              <NavLink to="/searchoffre" className="nav-main-link active"> <span className="nav-main-link-name">Recherche d'emploi</span></NavLink>
-                
-              </li>
-              
-              <li className="nav-main-item">
-              <NavLink to="/listeEntreprise" className="nav-main-link active"> <span className="nav-main-link-name"><span className="nav-main-link-name">Entreprises</span></span></NavLink>
-               
-              </li>
-              <li className="nav-main-item">
-                <a className="nav-main-link active" href="#">
-                  <span className="nav-main-link-name">Ressources</span>
-                </a>
-              </li>
-              <li className="nav-main-item">
-                <a className="nav-main-link active" href="#">
-                  <span className="nav-main-link-name">À propos de nous</span>
-                </a>
-              </li>
-              <li className="nav-main-item">
-                <a className="nav-main-link active" href="#">
-                  <span className="nav-main-link-name">Contact</span>
-                </a>
-              </li>
-              
-            </ul>
-          </div>
-       
-        </div>
-      </div>
+    <nav class="navbar navbar-expand-lg navbar-success fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Wonboard</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Offre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Coaching</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Profile</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Déconnexion</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
 
-
-    
-   
- 
-  </main>
   )
 }
 
--- src/composants/NavbarEntreprise.js
+++ src/composants/NavbarEntreprise.js
@@ -2,45 +2,30 @@ import React from 'react'
 
 const NavbarEntreprise = () => {
   return (
-    <header id="page-header">
-      <div className="content-header">
-        <div className="d-flex align-items-center">
-          <a className="fw-semibold fs-5 tracking-wider text-dual me-3" href="index.html"> <img width="120px"
-              src="assets/media/favicons/logo_large.jpg" alt="" className=""/> </a>
-          <div className="dropdown d-inline-block me-2">
-          </div>
-        </div>
-
-        <div className="d-flex align-items-center">
-          <div className="ms-2">
-            <div className="row items-push mb-4">
-              <div className="col-sm-6 col-xl-4">
-                <div className="dropdown">
-                  <button type="button" className="btn btn-outline-primary dropdown-toggle"
-                    id="dropdown-default-outline-primary" data-bs-toggle="dropdown" aria-haspopup="true"
-                    aria-expanded="false">
-                    Profile
-                  </button>
-                  <div className="dropdown-menu fs-sm" aria-labelledby="dropdown-default-outline-primary">
-                    <a className="dropdown-item" href="javascript:void(0)">Paramètre</a>
-                    <a className="dropdown-item" href="javascript:void(0)">Mes profils</a>
-                    <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="javascript:void(0)">Se déconnecter</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div id="page-header-loader" className="overlay-header bg-success">
-          <div className="content-header">
-            <div className="w-100 text-center">
-              <i className="fa fa-fw fa-circle-notch fa-spin text-primary"></i>
-            </div>
-          </div>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-success fixed-top">
+  <div class="container">
+    <a class="navbar-brand" href="#">Mon Site</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+      aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Mes offre</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Candidat</a>
+        </li>
+        
+        <li class="nav-item">
+          <a class="nav-link" href="#">Déconnexion</a>
+        </li>
+      </ul>
     </div>
-    </header>
+  </div>
+</nav>
+
   )
 }
 
